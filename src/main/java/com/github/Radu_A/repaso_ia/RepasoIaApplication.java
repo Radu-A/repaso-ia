f28d0cd4547b60a965fdb4f8cdf7126c72ba4106
Radu-A <elcastravete@gmail.com>
2026-05-20 17:03:02 +0200

    chore: create file structure

---
+++ src/main/java/com/github/Radu_A/repaso_ia/RepasoIaApplication.java
@@ -0,0 +1,13 @@
+package com.github.Radu_A.repaso_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasoIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Radu_A/repaso_ia/RepasoIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Radu_A.repaso_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasoIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
